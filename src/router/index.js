f868ddf66624f38927c8b206d66a407735b5ade4
AlvisWu <javacj02004@gmail.com>
2025-12-11 13:53:58 +0800

    Fix router for GitHub Pages - use hash history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAppStore } from '../stores/useAppStore.js'
 
 // Views
@@ -74,7 +74,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
